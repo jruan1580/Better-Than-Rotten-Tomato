232f003ab052c6a4dcb87402ca935554caa61c11
Jason Ruan <jason.ruan@lytx.com>
2021-08-19 15:36:18 -0700

    remove files

--- BetterThanRottenTomatoUI/better-than-rotten-tomato/src/App.js
+++ BetterThanRottenTomatoUI/better-than-rotten-tomato/src/App.js
@@ -1,17 +1,9 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Home';
-import NavigationBar from './Navbar';
-import Container from 'react-bootstrap/Container';
 
 function App() {
   return(
     <>
-      <NavigationBar />
-      <br/><br/>
-      <Container>
-        <Home/>
-      </Container>      
+      <h1>Hi</h1> 
     </>
   );  
 }
