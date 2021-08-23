4b716f840c88d3eeef0f1343e2e3ada541a65252
Jason Ruan <jason.ruan@lytx.com>
2021-08-23 13:55:03 -0700

    for movie list, used useReducer instead to consolidate all states

--- BetterThanRottenTomatoUI/better-than-rotten-tomato/src/Home/MovieList.js
+++ BetterThanRottenTomatoUI/better-than-rotten-tomato/src/Home/MovieList.js
@@ -3,17 +3,39 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import Pagination from 'react-bootstrap/Pagination';
-import { Fragment, useEffect, useRef, useState } from 'react';
+import { Fragment, useEffect, useReducer, useRef } from 'react';
 import AddMovie from './AddMovie';
 import { getMovies } from '../Services/MovieManagementService';
 import Loading from '../Loading/Loading';
 
+
+function reducer(state, action){
+    switch(action.type){
+        case 'setCurrentPage':
+            return {...state, currentPage: action.payload};
+        case 'setMoviesByRows':
+            return {...state, moviesByRows: action.payload};
+        case 'setTotalRecords':
+            return {...state, totalRecords: action.payload};
+        case 'setShowAddModal':
+            return {...state, showAddModal: action.payload};
+        case 'setShowLoading':
+            return {...state, showLoading: action.payload};
+        default:
+            throw new Error('action not defined');
+    }
+}
+
+const defaultState ={
+    currentPage: 1,
+    moviesByRows: [],
+    totalRecords: 0,
+    showAddModal: false,
+    showLoading: true
+};
+
 function MovieList({categories, search}){
-    const [currentPage, setCurrentPage] = useState(1);
-    const [moviesByRows, setMoviesByRows] = useState([]);
-    const [totalRecords, setTotalRecords] = useState(0);
-    const [showAddModal, setShowAddMovieModal] = useState(false);
-    const [showLoading, setLoadingModal] = useState(true);
+    const [state, dispatcher] = useReducer(reducer, defaultState);
 
     const offset = 6;
 
@@ -27,27 +49,27 @@ function MovieList({categories, search}){
                 //regrabbing because categories changed, if that is the case, reset page to 1
                 if (prevCategories.current !== categories){
                     prevCategories.current = categories;
-                    setCurrentPage(1);
+                    dispatcher({type: 'setCurrentPage', payload: 1});
                 }
 
                 //regrabbing because search changed, if that is the case, reset page to 1
                 if (prevSearch.current !== search){
                     prevSearch.current = search;
-                    setCurrentPage(1);
+                    dispatcher({type: 'setCurrentPage', payload: 1});
                 }
 
                 //regrabbing because add modal state change, if that is the case, only grab if its state change from open to close
-                if (prevAddModalState.current !== showAddModal){
+                if (prevAddModalState.current !== state.showAddModal){
                     let tmp = prevAddModalState.current;
-                    prevAddModalState.current = showAddModal;
+                    prevAddModalState.current = state.showAddModal;
                     //close to open, do not regrab
                     if (tmp === false && prevAddModalState.current === true){
                         return;
                     }                    
                 }
                 
-                setLoadingModal(true);
-                var movieResp = await getMovies(categories, search, currentPage, offset); 
+                dispatcher({type: 'setShowLoading', payload: true});
+                var movieResp = await getMovies(categories, search, state.currentPage, offset); 
                 var currMovieRow = [];
                 var allMoviesByRows = [];
 
@@ -66,25 +88,25 @@ function MovieList({categories, search}){
                     allMoviesByRows.push(currMovieRow);
                 }                
                 
-                setTotalRecords(movieResp.totalRecords);
-                setMoviesByRows(allMoviesByRows);
-                setLoadingModal(false);
+                dispatcher({type: 'setTotalRecords', payload: movieResp.totalRecords});
+                dispatcher({type: 'setMoviesByRows', payload: allMoviesByRows});
+                dispatcher({type: 'setShowLoading', payload: false});
             }catch(e){
                 alert('Failed to load movie with error: ' + e.message);
 
-                setLoadingModal(false);
+                dispatcher({type: 'setShowLoading', payload: false});
             }
         })()
-    }, [currentPage, categories, search, showAddModal]);
+    }, [state.currentPage, categories, search, state.showAddModal]);
 
     const setModalState = (state) =>{
-        setShowAddMovieModal(state);
+        dispatcher({type: 'setShowAddModal', payload: state});
     };
 
     return(
         <>
-            <Loading show={showLoading}/>
-            <AddMovie showModal={showAddModal} setModal={setModalState}/>
+            <Loading show={state.showLoading}/>
+            <AddMovie showModal={state.showAddModal} setModal={setModalState}/>
             <Row>
                 <Col lg="10">
                     <h1>Movies</h1>
@@ -94,7 +116,7 @@ function MovieList({categories, search}){
                 </Col>
             </Row><br/>
             {               
-                moviesByRows.map((movieRow, index) => {                    
+                state.moviesByRows.map((movieRow, index) => {                    
                     return (
                         <Fragment key={index+1}>
                             <Row>
@@ -115,10 +137,10 @@ function MovieList({categories, search}){
             }  
             <br/>
             <Pagination>
-                {(currentPage !== 1 && moviesByRows.length > 0) && <Pagination.First onClick={() => setCurrentPage(1)} />}
-                {(currentPage !== 1 && moviesByRows.length > 0) && <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)} />}
-                {(moviesByRows.length > 0 && currentPage !== ((totalRecords % offset === 0 && totalRecords !== 0) ? parseInt(totalRecords / offset) : (parseInt(totalRecords / offset) + 1))) && <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)}/>}
-                {(moviesByRows.length > 0 && currentPage !== ((totalRecords % offset === 0 && totalRecords !== 0) ? parseInt(totalRecords / offset) : (parseInt(totalRecords / offset) + 1))) &&<Pagination.Last onClick={() => setCurrentPage( ((totalRecords % offset === 0 && totalRecords !== 0) ? parseInt(totalRecords / offset) : (parseInt(totalRecords / offset) + 1)))}/>}
+                {(state.currentPage !== 1 && state.moviesByRows.length > 0) && <Pagination.First onClick={() => dispatcher({type: 'setCurrentPage', payload: 1})} />}
+                {(state.currentPage !== 1 && state.moviesByRows.length > 0) && <Pagination.Prev onClick={() => dispatcher({type: 'setCurrentPage', payload: (state.currentPage - 1)})} />}
+                {(state.moviesByRows.length > 0 && state.currentPage !== ((state.totalRecords % offset === 0 && state.totalRecords !== 0) ? parseInt(state.totalRecords / offset) : (parseInt(state.totalRecords / offset) + 1))) && <Pagination.Next onClick={() => dispatcher({type: 'setCurrentPage', payload: (state.currentPage + 1)}) }/>}
+                {(state.moviesByRows.length > 0 && state.currentPage !== ((state.totalRecords % offset === 0 && state.totalRecords !== 0) ? parseInt(state.totalRecords / offset) : (parseInt(state.totalRecords / offset) + 1))) &&<Pagination.Last onClick={() => dispatcher({type:'setCurrentPage', payload: ((state.totalRecords % offset === 0 && state.totalRecords !== 0) ? parseInt(state.totalRecords / offset) : (parseInt(state.totalRecords / offset) + 1))})}/>}
             </Pagination>
             
         </>
