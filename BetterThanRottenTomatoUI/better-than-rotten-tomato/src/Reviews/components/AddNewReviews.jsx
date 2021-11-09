79e3e56c3d8983102dea5339ace47ce8dc8f8b47
Anakaren <ak.rojas.07@gmail.com>
2021-11-09 12:17:09 -0800

    included state changes for form validation

--- BetterThanRottenTomatoUI/better-than-rotten-tomato/src/Reviews/components/AddNewReviews.jsx
+++ BetterThanRottenTomatoUI/better-than-rotten-tomato/src/Reviews/components/AddNewReviews.jsx
@@ -2,9 +2,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { addMovieReviews } from '../../Services/ReviewManagementService';
-import React, { Fragment } from 'react';
-import { useParams } from 'react-router-dom';
-import '../reviewsStyles.css';
+import React, { Fragment, useState } from 'react';
 
 class ReviewForm extends React.Component{
   constructor(props){
@@ -12,38 +10,72 @@ class ReviewForm extends React.Component{
     this.state = {
       username: '',
       rating: 0,
-      comment: ''
+      comment: '',
+      invalidForm: true
     }
   }
+  //validate form
+// should return formValid bool
+ validateForm = (event) => {
+   const name = event.target.name;
+   const value = event.target.value;
+
+   this.setState({[name]: value});
+};
+
+
+//on submit
+submitNewReviewForm(submitEvent) {
+  try {
+     addMovieReviews(
+      submitEvent.target.movieId.value,
+      submitEvent.target.username.value,
+      submitEvent.target.rating.value,
+      submitEvent.target.comment.value
+    );
+  } catch (e) {}
+};
+
   render(){
     return (
       <>
         <div className="container">
           <h3>Add new movie review</h3>
-          <form className="border p-5">
+          <form className="border p-4">
             <Row>
               <Col>
               <div className="form-group">
-                <label htmlFor="displayNameInput"> Display Name</label>
-                <input type="text" className="form-control" name="displayNameInput"/>
+                <input type="text" 
+                  className="form-control" 
+                  name="displayNameInput" 
+                  placeholder="Display Name" 
+                  value={this.state.username}
+                  onChange={(event) => this.validateForm(event)}/>
               </div>
               </Col>
               <Col>
               <div className="form-group">
-                <label htmlFor="ratingInput"> Rating </label>
-                <input className="form-control" name="ratingInput"/>
+                <input 
+                  className="form-control" 
+                  name="ratingInput" 
+                  placeholder="Rating" 
+                  value={this.state.rating}/>
               </div>
               </Col>
             </Row>
-            <Row>
+            <Row className='pt-3'>
               <div className="form-group">
-                <label htmlFor="commentInput">Comment</label>
-                <textarea className="form-control" name="commentInput"></textarea>
+                <textarea 
+                  className="form-control" 
+                  name="commentInput" 
+                  placeholder="Comment" 
+                  value={this.state.comment} 
+                  onChange={(event) => this.validateForm(event)}/>
               </div>
             </Row>
             <Row>
               <Col>
-                <Button type="submit" className="btn btn-dark mt-3" disabled={invalidForm}>
+                <Button type="submit" className="btn btn-dark mt-3" disabled={this.state.invalidForm}>
                   Submit
                 </Button>
               </Col>
@@ -57,19 +89,4 @@ class ReviewForm extends React.Component{
 
 export default ReviewForm;
 
-let invalidForm = true;
-//validate form
-// should return formValid bool
-const validateForm = () => {};
 
-//on submit
-async function submitNewReviewForm(submitEvent) {
-  try {
-    await addMovieReviews(
-      submitEvent.target.movieId.value,
-      submitEvent.target.username.value,
-      submitEvent.target.rating.value,
-      submitEvent.target.comment.value
-    );
-  } catch (e) {}
-};
