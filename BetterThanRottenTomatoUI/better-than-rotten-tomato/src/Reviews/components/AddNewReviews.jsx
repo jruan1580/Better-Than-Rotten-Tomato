53a76ce14f3e4053057c84738c61552b2fc47357
Anakaren <ak.rojas.07@gmail.com>
2021-11-11 04:33:30 -0800

    split validation for fields and form

--- BetterThanRottenTomatoUI/better-than-rotten-tomato/src/Reviews/components/AddNewReviews.jsx
+++ BetterThanRottenTomatoUI/better-than-rotten-tomato/src/Reviews/components/AddNewReviews.jsx
@@ -2,41 +2,72 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { addMovieReviews } from '../../Services/ReviewManagementService';
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 
-class ReviewForm extends React.Component{
-  constructor(props){
+class ReviewForm extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       username: '',
       rating: 0,
       comment: '',
-      invalidForm: true
-    }
+      invalidForm: true,
+      invalidDisplayName: true,
+      invalidComment: true,
+      invalidRating: true,
+    };
+
+    this.validateFields = this.validateFields.bind(this);
   }
-  //validate form
-// should return formValid bool
- validateForm = (event) => {
-   const name = event.target.name;
-   const value = event.target.value;
 
-   this.setState({[name]: value});
-};
+  validateFields = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
 
+    switch (name) {
+      case 'displayNameInput':
+        this.setState({ username: value });
+        this.state.invalidDisplayName = value !== '' ? false : true;
+        break;
+      case 'ratingInput':
+        this.setState({ rating: value });
+        this.state.invalidRating = value > 0 ? false : true;
+        break;
+      case 'commentInput':
+        this.setState({ comment: value });
+        this.state.invalidComment = value !== '' ? false : true;
+        break;
+    }
 
-//on submit
-submitNewReviewForm(submitEvent) {
-  try {
-     addMovieReviews(
-      submitEvent.target.movieId.value,
-      submitEvent.target.username.value,
-      submitEvent.target.rating.value,
-      submitEvent.target.comment.value
-    );
-  } catch (e) {}
-};
+    this.validateForm();
+  };
 
-  render(){
+  validateForm = () => {
+    console.log(`dn ${this.state.invalidDisplayName}`);console.log(`iR ${this.state.invalidRating}`);console.log(`comment ${this.state.invalidComment}`);
+    console.log(`invalidForm ${this.state.invalidForm}`);
+
+    const invalidFields = (this.state.invalidDisplayName && this.state.invalidRating && this.state.invalidComment);
+    console.log(`invalidFields ${this.state.invalidFields}`);
+    this.setState({
+      invalidForm:
+      invalidFields
+    });
+    console.log(`invalidForm ${this.state.invalidForm}`);
+  };
+
+  //on submit
+  submitNewReviewForm(submitEvent) {
+    try {
+      addMovieReviews(
+        submitEvent.target.movieId.value,
+        submitEvent.target.username.value,
+        submitEvent.target.rating.value,
+        submitEvent.target.comment.value
+      );
+    } catch (e) {}
+  }
+
+  render() {
     return (
       <>
         <div className="container">
@@ -44,38 +75,47 @@ submitNewReviewForm(submitEvent) {
           <form className="border p-4">
             <Row>
               <Col>
-              <div className="form-group">
-                <input type="text" 
-                  className="form-control" 
-                  name="displayNameInput" 
-                  placeholder="Display Name" 
-                  value={this.state.username}
-                  onChange={(event) => this.validateForm(event)}/>
-              </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="displayNameInput"
+                    placeholder="Display Name"
+                    value={this.state.username}
+                    onChange={(event) => this.validateFields(event)}
+                  />
+                </div>
               </Col>
               <Col>
-              <div className="form-group">
-                <input 
-                  className="form-control" 
-                  name="ratingInput" 
-                  placeholder="Rating" 
-                  value={this.state.rating}/>
-              </div>
+                <div className="form-group">
+                  <input
+                    className="form-control"
+                    name="ratingInput"
+                    placeholder="Rating"
+                    value={this.state.rating}
+                    onChange={(event) => this.validateFields(event)}
+                  />
+                </div>
               </Col>
             </Row>
-            <Row className='pt-3'>
+            <Row className="pt-3">
               <div className="form-group">
-                <textarea 
-                  className="form-control" 
-                  name="commentInput" 
-                  placeholder="Comment" 
-                  value={this.state.comment} 
-                  onChange={(event) => this.validateForm(event)}/>
+                <textarea
+                  className="form-control"
+                  name="commentInput"
+                  placeholder="Comment"
+                  value={this.state.comment}
+                  onChange={(event) => this.validateFields(event)}
+                />
               </div>
             </Row>
             <Row>
               <Col>
-                <Button type="submit" className="btn btn-dark mt-3" disabled={this.state.invalidForm}>
+                <Button
+                  type="submit"
+                  className="btn btn-dark mt-3"
+                  disabled={this.state.invalidForm}
+                >
                   Submit
                 </Button>
               </Col>
@@ -84,9 +124,7 @@ submitNewReviewForm(submitEvent) {
         </div>
       </>
     );
-  };
+  }
 }
 
 export default ReviewForm;
-
-
